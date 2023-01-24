2d5e8aaa8e8f640874945030394bdd98df553a27
Ravi Panchal <mpravikumar04@gmail.com>
2023-01-24 15:26:46 +0530

    Added alerts context, state and reducer

--- client/src/App.js
+++ client/src/App.js
@@ -2,30 +2,35 @@ import React, { Fragment } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
+import Alerts from "./components/layout/Alerts";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import Register from "./components/auth/Register";
 import Login from './components/auth/Login';
 import ContactState from "./context/contact/ContactState";
 import AuthState from "./context/auth/AuthState";
+import AlertState from "./context/alert/AlertState";
 
 function App() {
   return (
     <AuthState>
       <ContactState>
-        <Router>
-          <Fragment>
-            <Navbar />
-            <div className="container">
-              <Routes>
-                <Route exact path="/" element={<Home />} />
-                <Route exact path="/about" element={<About />} />
-                <Route exact path="/register" element={<Register />} />
-                <Route exact path="/login" element={<Login />} />
-              </Routes>
-            </div>
-          </Fragment>
-        </Router>
+        <AlertState>
+          <Router>
+            <Fragment>
+              <Navbar />
+              <div className="container">
+                <Alerts />
+                <Routes>
+                  <Route exact path="/" element={<Home />} />
+                  <Route exact path="/about" element={<About />} />
+                  <Route exact path="/register" element={<Register />} />
+                  <Route exact path="/login" element={<Login />} />
+                </Routes>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
       </ContactState>
     </AuthState>
   );
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,12 +1,26 @@
 import React from "react";
-import PropTypes from 'prop-types';
-
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 const Navbar = ({ title, icon }) => {
   return (
     <div className="navbar bg-primary">
       <h1>
-        <i className={icon}> {title}</i>
+        <i className={icon} /> {title}
       </h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
     </div>
   );
 };
@@ -18,9 +32,7 @@ Navbar.defaultProps = {
 
 Navbar.propTypes = {
   title: PropTypes.string.isRequired,
-  icon: PropTypes.string
+  icon: PropTypes.string,
 };
-
-
 
 export default Navbar;
