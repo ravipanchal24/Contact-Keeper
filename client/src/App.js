ce6e2699ad6c0eb49c33350a580dbf67d9b76b48
Ravi Panchal <mpravikumar04@gmail.com>
2023-01-24 14:20:52 +0530

    Added Register user component

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
+import Register from "./components/auth/Register";
 import ContactState from "./context/contact/ContactState";
 import AuthState from "./context/auth/AuthState";
 function App() {
@@ -17,6 +18,7 @@ function App() {
               <Routes>
                 <Route exact path="/" element={<Home />} />
                 <Route exact path="/about" element={<About />} />
+                <Route exact path="/register" element={<Register />} />
               </Routes>
             </div>
           </Fragment>
